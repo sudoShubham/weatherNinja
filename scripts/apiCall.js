17925da6c8aea608d998a97aad9028f2ba41b107
sudoShubham <shubhamp2017@gmail.com>
2021-09-30 13:06:28 +0530

    implemented class version of app

--- scripts/apiCall.js
+++ scripts/apiCall.js
@@ -1,34 +1,25 @@
-const key = "WTI7K1wAI0sLhh4LdS8X9xl5z6GJ24Rj";
+class Forecast {
+  constructor(){
+    this.key = 'WTI7K1wAI0sLhh4LdS8X9xl5z6GJ24Rj';
+    this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  }
+  async updateCity(city){
+    const cityDets = await this.getCity(city);
+    const weather = await this.getWeather(cityDets.Key);
+    return { cityDets, weather };
+  }
+  async getWeather(id){
+    const query = `${id}?apikey=${this.key}`;
+    const response = await fetch(this.weatherURI + query);
+    const data = await response.json();
+    return data[0];
+  }
+  async getCity(city){
+    const query = `?apikey=${this.key}&q=${city}`;
+    const response = await fetch(this.cityURI + query);
+    const data = await response.json();
+    return data[0];
+  }
 
-const getWeather = async (id) => {
-  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
-  const query = `${id}/?apikey=${key}`;
-  const requestLink = base + query;
-  const response = await fetch(requestLink);
-  const data = await response.json();
-  //   console.log(data);
-  return data[0];
-};
 
-const getCity = async (city) => {
-  const base = `http://dataservice.accuweather.com/locations/v1/cities/search`;
-  const query = `?apikey=${key}&q=${city}`;
-  const requestLink = base + query;
-  const response = await fetch(requestLink);
-  const data = await response.json();
-  //   console.log(data[0].Key);
-  return data[0];
-};
-
-// getCity("Nashik")
-//   .then((data) => {
-//     return getWeather(data.Key);
-//   })
-//   .then((data) => {
-//     console.log(data);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
-
-// getWeather(189304);
--- scripts/app.js
+++ scripts/app.js
@@ -1,70 +1,57 @@
-const cityForm = document.querySelector("form");
-const card = document.querySelector(".card");
-const details = document.querySelector(".details");
-const time = document.querySelector("img.time");
-const icon = document.querySelector(".icon img");
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+const forecast = new Forecast();
 
 const updateUI = (data) => {
-  //   const cityDetails = data.cityDetails;
-  //   const weatherDetails = data.weatherDetails;
+  // destructure properties
+  const { cityDets, weather } = data;
 
-  const { cityDetails, weatherDetails } = data;
-
+  // update details template
   details.innerHTML = `
-         <h5 class="my-3">${cityDetails.EnglishName}</h5>
-            <div class="my-3">${weatherDetails.WeatherText}</div>
-            <div class="display-4 my-4">
-                <span>${weatherDetails.Temperature.Metric.Value}</span>
-                <span>&deg;C</span>
-            </div>
-    
-    `;
-  if (card.classList.contains("d-none")) {
-    card.classList.remove("d-none");
+    <h5 class="my-3">${cityDets.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+      <span>${weather.Temperature.Metric.Value}</span>
+      <span>&deg;C</span>
+    </div>
+  `;
+
+  // update the night/day & icon images
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute('src', iconSrc);
+  
+  const timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
+  time.setAttribute('src', timeSrc);
+
+  // remove the d-none class if present
+  if(card.classList.contains('d-none')){
+    card.classList.remove('d-none');
   }
-
-  let imgSrc = weatherDetails.IsDayTime ? "./img/day.svg" : "./img/night.svg";
-  //   if (weatherDetails.IsDayTime) {
-  //     imgSrc = "./img/day.svg";
-  //   } else {
-  //     imgSrc = "./img/night.svg";
-  //   }
-
-  time.setAttribute("src", imgSrc);
-
-  const iconSrc = `./img/icons/${weatherDetails.WeatherIcon}.svg`;
-  icon.setAttribute("src", iconSrc);
 };
 
-const updateCity = async (city) => {
-  //   console.log(city);
-  const cityDetails = await getCity(city);
-  const weatherDetails = await getWeather(cityDetails.Key);
-
-  return {
-    // cityDetails: cityDetails,
-    // weatherDetails: weatherDetails,
-
-    cityDetails,
-    weatherDetails,
-  };
-};
-
-cityForm.addEventListener("submit", (e) => {
+cityForm.addEventListener('submit', e => {
+  // prevent default action
   e.preventDefault();
+  
+  // get city value
   const city = cityForm.city.value.trim();
   cityForm.reset();
 
-  updateCity(city)
-    .then((data) => {
-      updateUI(data);
-    })
-    .catch((err) => console.log(err));
-  localStorage.setItem("city", city);
+  // update the ui with new city
+  forecast.updateCity(city)
+    .then(data => updateUI(data))
+    .catch(err => console.log(err));
+
+  // set local storage
+  localStorage.setItem('city', city);
+
 });
 
-if (localStorage.getItem("city")) {
-  updateCity(localStorage.getItem("city"))
-    .then((data) => updateUI(data))
-    .catch((err) => console.log(err));
+if(localStorage.getItem('city')){
+  forecast.updateCity(localStorage.getItem('city'))
+    .then(data => updateUI(data))
+    .catch(err => console.log(err));
 }
